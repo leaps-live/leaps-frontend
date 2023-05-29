a609c5c1273e7018a6e7047cfe3a278323adc9e6
Julius <juliuscecilia33@gmail.com>
2023-05-29 13:15:26 -0700

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:leaps_frontend/screens/onboarding_screen.dart';
+import 'package:leaps_frontend/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +14,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LEAPS = Live-streaming Engagement for All People and Sports',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Text(
-          'LEAPS = Live-streaming Engagement for All People and Sports'),
+      theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: backgroundColor,
+          appBarTheme: AppBarTheme.of(context).copyWith(
+              backgroundColor: backgroundColor,
+              elevation: 0,
+              titleTextStyle: const TextStyle(
+                  color: primaryColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600),
+              iconTheme: const IconThemeData(color: primaryColor))),
+      home: const OnboardingScreen(),
     );
   }
 }
