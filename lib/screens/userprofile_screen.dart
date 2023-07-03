245cb467fe3e80fb22c1bc7e33ef18436606b4f9
Julius <juliuscecilia33@gmail.com>
2023-07-03 16:38:31 -0700

    Update userprofile_screen.dart

--- lib/screens/userprofile_screen.dart
+++ lib/screens/userprofile_screen.dart
@@ -44,79 +44,32 @@ class HeroBar extends StatelessWidget {
               borderRadius: BorderRadius.circular(10),
             ),
           ),
-          child: Stack(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Positioned(
-                left: 59,
-                top: 139,
-                child: Text(
-                  'Edit Profile',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(0xFF272727),
-                    fontSize: 17,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 20,
-                top: 186,
-                child: Container(
-                  width: 23,
-                  height: 23,
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 2.40, vertical: 0.96),
-                  clipBehavior: Clip.antiAlias,
-                  decoration: const BoxDecoration(),
-                  child: const Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    // children: [
-                    // ,
-                    // ],
-                  ),
-                ),
-              ),
-              const Positioned(
-                left: 59,
-                top: 187,
-                child: Text(
-                  'Settings',
-                  style: TextStyle(
-                    color: Color(0xFF282828),
-                    fontSize: 17,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 20,
-                top: 138,
-                child: Container(
-                  width: 23,
-                  height: 23,
-                  clipBehavior: Clip.antiAlias,
-                  decoration: const BoxDecoration(),
-                  child: const Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: 23,
-                        height: 23,
-                        // child: Stack(children: [
-                        // ,
-                        // ]),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.edit_square,
+                      size: 25.0, color: Colors.black),
+                  label: const Text('Edit Profile',
+                      style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 21,
+                          color: Colors.black),
+                      textAlign: TextAlign.center)),
+              // const SizedBox(height: 60),
+              TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.settings,
+                      size: 25.0, color: Colors.black),
+                  label: const Text('Settings',
+                      style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 21,
+                          color: Colors.black),
+                      textAlign: TextAlign.center))
             ],
           ),
         ),
