056d333abec3533dcbe3ebf128c22bc47dfa4c68
Julius <juliuscecilia33@gmail.com>
2023-07-19 18:49:19 -0700

    Update careerprofile_screen.dart

--- lib/screens/career/careerprofile_screen.dart
+++ lib/screens/career/careerprofile_screen.dart
@@ -107,6 +107,70 @@ class HeroContent extends StatelessWidget {
                   ],
                 )
               ]),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 18.0, vertical: 18.0),
+                width: MediaQuery.of(context).size.width * 0.90,
+                // height: 230,
+                decoration: ShapeDecoration(
+                  color: const Color(0xFFFEFEFE),
+                  shape: RoundedRectangleBorder(
+                    side:
+                        const BorderSide(width: 0.25, color: Color(0xFFE6E6E6)),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://picsum.photos/id/237/200/300'),
+                          ),
+                          Column(children: [
+                            Text(
+                              'Ruolin',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 25),
+                              textAlign: TextAlign.left,
+                            ),
+                            Text(
+                              'Ruov',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.normal, fontSize: 15),
+                              textAlign: TextAlign.left,
+                            )
+                          ])
+                        ]),
+                    TextButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(Icons.edit_square,
+                            size: 21.0, color: Colors.black),
+                        label: const Text('Edit Profile',
+                            style: TextStyle(
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.normal,
+                                fontSize: 17,
+                                color: Colors.black),
+                            textAlign: TextAlign.center)),
+                    TextButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(Icons.settings,
+                            size: 21.0, color: Colors.black),
+                        label: const Text('Settings',
+                            style: TextStyle(
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.normal,
+                                fontSize: 17,
+                                color: Colors.black),
+                            textAlign: TextAlign.center))
+                  ],
+                ),
+              ),
             ],
           ),
         )
