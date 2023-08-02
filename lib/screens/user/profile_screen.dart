465b01ad67384f0e7560588a6a218ed651d58954
tangjingyu0812 <jingyuta@usc.edu>
2023-08-02 10:46:42 +0800

    feat: add setting page and  showing or not

--- lib/screens/user/profile_screen.dart
+++ lib/screens/user/profile_screen.dart
@@ -49,7 +49,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   void initState() {
-    print("init!!!!!!!!!!!!!!!!!!!!!!!");
     super.initState();
     _getUserData();
   }
--- lib/screens/user/settings_screen.dart
+++ lib/screens/user/settings_screen.dart
@@ -5,16 +5,41 @@ import 'package:leaps_frontend/screens/user/profile_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
   static const routeName = '/settings';
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool isLogin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserData();
+  }
+
+  Future<void> _getUserData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? userid = prefs.getString('userid');
+    print(userid);
+    if (userid != null) {
+      setState(() {
+        isLogin = true;
+      });
+    }
+    print(isLogin);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Settings")),
+        appBar: AppBar(title: const Text("Settings")),
         body: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -23,6 +48,7 @@ class SettingsScreen extends StatelessWidget {
                   'Account Security',
                   style: TextStyle(color: Colors.black, fontSize: 19),
                 ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
                 trailing: const Icon(
                   Icons.arrow_forward_ios,
                   color: Colors.black,
@@ -36,13 +62,14 @@ class SettingsScreen extends StatelessWidget {
               const Text("NOTIFICATION",
                   style: TextStyle(
                       color: Colors.black,
-                      fontSize: 17,
+                      fontSize: 15,
                       fontWeight: FontWeight.bold)),
               ListTile(
                 title: const Text(
-                  'Account Security',
+                  'Manage Settings',
                   style: TextStyle(color: Colors.black, fontSize: 19),
                 ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
                 trailing: const Icon(
                   Icons.arrow_forward_ios,
                   color: Colors.black,
@@ -50,37 +77,109 @@ class SettingsScreen extends StatelessWidget {
                 ),
                 onTap: () {},
               ),
-              TextButton(
-                onPressed: () {
-                  userSignOut();
-                  Navigator.pushNamed(context, MainScreen.routeName);
-                },
-                child: const Text(
-                  'Log out',
-                  style: TextStyle(fontSize: 19, color: Colors.black),
+              const SizedBox(
+                height: 16,
+              ),
+              const Text(
+                'PRIVACY',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+              ListTile(
+                title: const Text(
+                  'Blocked Contacts',
+                  style: TextStyle(color: Colors.black, fontSize: 19),
                 ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+                trailing: const Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.black,
+                  size: 20,
+                ),
+                onTap: () {},
               ),
               const SizedBox(
-                height: 80,
+                height: 16,
               ),
-              TextButton(
-                onPressed: () {
-                  deleteAccount();
-                  Navigator.pushNamed(context, MainScreen.routeName);
-                },
-                child: const Text(
-                  'Delete account',
-                  style: TextStyle(fontSize: 19, color: Colors.black),
+              const Text("ABOUT",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold)),
+              ListTile(
+                title: const Text(
+                  'Privacy Policy',
+                  style: TextStyle(color: Colors.black, fontSize: 19),
                 ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+                trailing: const Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.black,
+                  size: 20,
+                ),
+                onTap: () {},
               ),
+              ListTile(
+                title: const Text(
+                  'Support Us',
+                  style: TextStyle(color: Colors.black, fontSize: 19),
+                ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+                trailing: const Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.black,
+                  size: 20,
+                ),
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text(
+                  'Contact Us',
+                  style: TextStyle(color: Colors.black, fontSize: 19),
+                ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+                trailing: const Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.black,
+                  size: 20,
+                ),
+                onTap: () {},
+              ),
+              if (isLogin)
+                ListTile(
+                  title: const Text(
+                    'Log Out',
+                    style: TextStyle(color: Colors.black, fontSize: 19),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+                  onTap: () {
+                    userLogOut();
+                    Navigator.pushNamed(context, MainScreen.routeName);
+                  },
+                ),
+              if (isLogin)
+                ListTile(
+                  title: const Text(
+                    'Delete Account',
+                    style: TextStyle(color: Colors.black, fontSize: 19),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+                  onTap: () {
+                    deleteAccount();
+                    Navigator.pushNamed(context, MainScreen.routeName);
+                  },
+                ),
             ],
           ),
         ));
   }
 
-  void userSignOut() async {
+  void userLogOut() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.remove('user');
+    await prefs.remove('userid');
     Fluttertoast.showToast(
       msg: "Sign out successfully",
       toastLength: Toast.LENGTH_SHORT,
@@ -106,6 +205,7 @@ class SettingsScreen extends StatelessWidget {
         print('Account deleted successfully!');
 
         await prefs.remove('user');
+        await prefs.remove('userid');
 
         Fluttertoast.showToast(
           msg: "Account deleted successfully",
