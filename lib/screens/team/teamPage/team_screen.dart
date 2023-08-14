7f4de9b07c6555fc174ea53f34c48be7492b8713
tangjingyu0812 <jingyuta@usc.edu>
2023-08-14 10:18:48 +0800

    feat: add initstate for teampage

--- lib/screens/team/teamPage/team_screen.dart
+++ lib/screens/team/teamPage/team_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:leaps_frontend/screens/team/teamPage/team_index.dart';
 import 'package:leaps_frontend/screens/team/teamPage/team_tabbar.dart';
+import 'package:http/http.dart' as http;
 
 class TeamScreen extends StatefulWidget {
   const TeamScreen({super.key});
@@ -11,6 +12,40 @@ class TeamScreen extends StatefulWidget {
 }
 
 class _TeamScreenState extends State<TeamScreen> {
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserData();
+  }
+
+  Future<void> _getUserData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    String teamid = '24dbe493-6e10-49d8-b8da-ae26f9dcbe4c';
+
+    try {
+      final response =
+          await http.get(Uri.parse('http://localhost:8080/team/get/$teamid'));
+
+      print(response.body);
+
+      if (response.statusCode == 200) {
+      } else {
+        print('fail request ${response.statusCode}');
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
