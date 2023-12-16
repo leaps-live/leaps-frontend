5cba14ab1297ba18d4c198916cbba7f937bf7fb8
Julius <juliuscecilia33@gmail.com>
2023-12-16 01:14:00 -0700

    added upload button

--- lib/screens/playground/s3_upload_test_screen.dart
+++ lib/screens/playground/s3_upload_test_screen.dart
@@ -87,70 +87,17 @@ class _S3UploadScreenState extends State<S3UploadScreen> {
             ),
             Expanded(
               child: buildGridView(),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 30.0),
+              child: ElevatedButton(
+                onPressed: loadAssets,
+                child: const Text("Upload"),
+              ),
             )
           ],
         ),
       ),
     );
   }
-
-  // bool isApiCallProcess = false;
-  // String singleImageFile = "";
-  // List<String> selectedMultiImages = [];
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //       appBar: AppBar(title: const Text("S3 Uploading")),
-  //       body: ProgressHUD(
-  //           child: uploadUI(),
-  //           inAsyncCall: isApiCallProcess,
-  //           opacity: .3,
-  //           key: UniqueKey()));
-  // }
-
-  // uploadUI() {
-  //   return Column(
-  //     mainAxisAlignment: MainAxisAlignment.center,
-  //     crossAxisAlignment: CrossAxisAlignment.start,
-  //     children: [
-  //       const Text("Single Image",
-  //           style: TextStyle(fontWeight: FontWeight.bold)),
-  //       SizedBox(
-  //         child: MultiImagePicker(
-  //             totalImages: 1,
-  //             imageSource: ImagePickSource.gallery,
-  //             onImageChanged: (images) {
-  //               singleImageFile = images[0].imageFile;
-  //             }),
-  //       ),
-  //       // const Text("Multiple Image",
-  //       //     style: TextStyle(fontWeight: FontWeight.bold)),
-  //       // SizedBox(
-  //       //   child: MultiImagePicker(
-  //       //       totalImages: 5,
-  //       //       imageSource: ImagePickSource.gallery,
-  //       //       initialValue: const [],
-  //       //       onImageChanged: (images) {
-  //       //         selectedMultiImages = [];
-
-  //       //         images.forEach((image) {
-  //       //           if (image is ImageUploadModel) {
-  //       //             selectedMultiImages.add(image.imageFile);
-  //       //           }
-  //       //         });
-
-  //       //         singleImageFile = images[0].imageFile;
-  //       //       }),
-  //       // ),
-  //       Center(
-  //         child: FormHelper.submitButton("Upload", () {},
-  //             btnColor: Colors.redAccent,
-  //             borderColor: Colors.redAccent,
-  //             txtColor: Colors.white,
-  //             borderRadius: 10),
-  //       )
-  //     ],
-  //   );
-  // }
 }
