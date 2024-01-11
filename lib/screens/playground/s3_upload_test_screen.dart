73afe2309526fa32e9eb1284e7f35b3f51f21939
Julius <juliuscecilia33@gmail.com>
2024-01-10 16:31:55 -0800

    started image upload playground

--- lib/screens/playground/s3_upload_test_screen.dart
+++ lib/screens/playground/s3_upload_test_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:snippet_coder_utils/ProgressHUD.dart';
+import 'package:snippet_coder_utils/multi_images_utils.dart';
 // import 'dart:async';
 // import 'package:multi_image_picker2/multi_image_picker2.dart';
 
@@ -11,70 +13,41 @@ class S3UploadScreen extends StatefulWidget {
 }
 
 class _S3UploadScreenState extends State<S3UploadScreen> {
-  // List<Asset> images = <Asset>[];
-  // String _error = 'No Error Dectected';
-
   @override
   void initState() {
     super.initState();
   }
 
-  // Widget buildGridView() {
-  //   return GridView.count(
-  //     crossAxisCount: 3,
-  //     children: List.generate(images.length, (index) {
-  //       Asset asset = images[index];
-  //       return AssetThumb(
-  //         asset: asset,
-  //         width: 300,
-  //         height: 300,
-  //       );
-  //     }),
-  //   );
-  // }
+  uploadUI() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("Single Image",
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        MultiImagePicker(
+            totalImages: 1,
+            onImageChanged: (images) {
+              singleImageFile = images[0].image;
+            },
+            imageSource: ImagePickSource.gallery)
+      ],
+    );
+  }
 
-  // Future<void> loadAssets() async {
-  //   List<Asset> resultList = <Asset>[];
-  //   String error = 'No Error Detected';
-
-  //   try {
-  //     resultList = await MultiImagePicker.pickImages(
-  //       maxImages: 300,
-  //       enableCamera: true,
-  //       selectedAssets: images,
-  //       cupertinoOptions: const CupertinoOptions(
-  //         takePhotoIcon: "chat",
-  //         doneButtonTitle: "Fatto",
-  //       ),
-  //       materialOptions: const MaterialOptions(
-  //         actionBarColor: "#abcdef",
-  //         actionBarTitle: "Example App",
-  //         allViewTitle: "All Photos",
-  //         useDetailsView: false,
-  //         selectCircleStrokeColor: "#000000",
-  //       ),
-  //     );
-  //   } on Exception catch (e) {
-  //     error = e.toString();
-  //   }
-
-  //   // If the widget was removed from the tree while the asynchronous platform
-  //   // message was in flight, we want to discard the reply rather than calling
-  //   // setState to update our non-existent appearance.
-  //   if (!mounted) return;
-
-  //   setState(() {
-  //     images = resultList;
-  //     _error = error;
-  //   });
+  bool isApiCallprocess = false;
+  String singleImageFile = "";
+  List<String> selectedMultiImages = [];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("S3 Uploading")),
-        body: Column(),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text("S3 Uploading")),
+      body: ProgressHUD(
+          key: UniqueKey(),
+          child: uploadUI(),
+          inAsyncCall: isApiCallprocess,
+          opacity: .3),
     );
   }
 }
