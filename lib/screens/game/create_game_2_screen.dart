aef195d7d4c589a83053845cfffdd5f253572f7f
Julius <juliuscecilia33@gmail.com>
2024-01-30 23:59:36 -0800

    minutes and quarter selector

--- lib/screens/game/create_game_2_screen.dart
+++ lib/screens/game/create_game_2_screen.dart
@@ -22,6 +22,9 @@ class _CreateGameTwoScreenState extends State<CreateGameTwoScreen> {
   DateTime dateTime = DateTime.now();
   DateTime justTime = DateTime.now();
 
+  int numberOfQuarters = 0;
+  int minutesPerQuarter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -135,7 +138,9 @@ class _CreateGameTwoScreenState extends State<CreateGameTwoScreen> {
                         builder: (BuildContext context) => SizedBox(
                               height: 250,
                               child: CupertinoDatePicker(
-                                  backgroundColor: Colors.white,
+                                  backgroundColor: CupertinoColors
+                                      .systemBackground
+                                      .resolveFrom(context),
                                   initialDateTime: dateTime,
                                   onDateTimeChanged: (DateTime newTime) {
                                     setState(() => dateTime = newTime);
@@ -167,7 +172,9 @@ class _CreateGameTwoScreenState extends State<CreateGameTwoScreen> {
                         builder: (BuildContext context) => SizedBox(
                               height: 250,
                               child: CupertinoDatePicker(
-                                  backgroundColor: Colors.white,
+                                  backgroundColor: CupertinoColors
+                                      .systemBackground
+                                      .resolveFrom(context),
                                   initialDateTime: justTime,
                                   onDateTimeChanged: (DateTime newTime) {
                                     setState(() => justTime = newTime);
@@ -205,7 +212,32 @@ class _CreateGameTwoScreenState extends State<CreateGameTwoScreen> {
               height: 8,
             ),
             ElevatedButton(
-              onPressed: () => {},
+              onPressed: () => {
+                showCupertinoModalPopup(
+                    context: context,
+                    builder: (_) => SizedBox(
+                        width: double.infinity,
+                        height: 250,
+                        child: CupertinoPicker(
+                          backgroundColor: CupertinoColors.systemBackground
+                              .resolveFrom(context),
+                          itemExtent: 30,
+                          scrollController:
+                              FixedExtentScrollController(initialItem: 0),
+                          onSelectedItemChanged: (int value) {
+                            setState(() {
+                              numberOfQuarters = value;
+                            });
+                          },
+                          children: const [
+                            Text('0'),
+                            Text('1'),
+                            Text('2'),
+                            Text('3'),
+                            Text('4')
+                          ],
+                        )))
+              },
               style: ElevatedButton.styleFrom(
                   backgroundColor: tagColor,
                   elevation: 0.0,
@@ -215,8 +247,8 @@ class _CreateGameTwoScreenState extends State<CreateGameTwoScreen> {
                   ),
                   padding:
                       const EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
-              child: const Text("1",
-                  style: TextStyle(
+              child: Text(numberOfQuarters.toString(),
+                  style: const TextStyle(
                       color: secondaryTextColor, fontWeight: FontWeight.bold)),
             ),
             const SizedBox(
@@ -230,7 +262,48 @@ class _CreateGameTwoScreenState extends State<CreateGameTwoScreen> {
               height: 8,
             ),
             ElevatedButton(
-              onPressed: () => {},
+              onPressed: () => {
+                showCupertinoModalPopup(
+                    context: context,
+                    builder: (_) => SizedBox(
+                        width: double.infinity,
+                        height: 250,
+                        child: CupertinoPicker(
+                          backgroundColor: CupertinoColors.systemBackground
+                              .resolveFrom(context),
+                          itemExtent: 30,
+                          scrollController:
+                              FixedExtentScrollController(initialItem: 1),
+                          onSelectedItemChanged: (int value) {
+                            setState(() {
+                              minutesPerQuarter = value;
+                            });
+                          },
+                          children: const [
+                            Text('0'),
+                            Text('1'),
+                            Text('2'),
+                            Text('3'),
+                            Text('4'),
+                            Text('5'),
+                            Text('6'),
+                            Text('7'),
+                            Text('8'),
+                            Text('9'),
+                            Text('10'),
+                            Text('11'),
+                            Text('12'),
+                            Text('13'),
+                            Text('14'),
+                            Text('15'),
+                            Text('16'),
+                            Text('17'),
+                            Text('18'),
+                            Text('19'),
+                            Text('20'),
+                          ],
+                        )))
+              },
               style: ElevatedButton.styleFrom(
                   backgroundColor: tagColor,
                   elevation: 0.0,
@@ -240,8 +313,8 @@ class _CreateGameTwoScreenState extends State<CreateGameTwoScreen> {
                   ),
                   padding:
                       const EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
-              child: const Text("1",
-                  style: TextStyle(
+              child: Text(minutesPerQuarter.toString(),
+                  style: const TextStyle(
                       color: secondaryTextColor, fontWeight: FontWeight.bold)),
             ),
             Expanded(
