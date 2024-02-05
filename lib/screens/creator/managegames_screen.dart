5416542127e9c1e22c1da0de97a500b18bad5cf6
Julius <juliuscecilia33@gmail.com>
2024-02-04 16:54:34 -0800

    Update managegames_screen.dart

--- lib/screens/creator/managegames_screen.dart
+++ lib/screens/creator/managegames_screen.dart
@@ -25,19 +25,251 @@ class _ManageGamesScreenState extends State<ManageGamesScreen> {
         ),
         iconTheme: const IconThemeData(color: Colors.black),
       ),
-      body: const Padding(
+      body: Padding(
         padding: EdgeInsets.all(18),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               "Upcoming",
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 19),
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
             ),
-            SizedBox(height: 20),
-            Text(
+            const SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              // height: 100,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(8)),
+              child: Material(
+                type: MaterialType.transparency,
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.black26,
+                  child: Column(mainAxisSize: MainAxisSize.min, children: [
+                    Container(
+                      width: double.infinity,
+                      height: 40,
+                      decoration: const BoxDecoration(
+                        color: basketball,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(8.0),
+                          topLeft: Radius.circular(8.0),
+                        ),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Remix.basketball_fill,
+                                      color: Colors.white,
+                                      size: 18,
+                                    ),
+                                    SizedBox(width: 8),
+                                    Text(
+                                      "Tuesday, March 5",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ],
+                                ),
+                                Spacer(),
+                                Icon(
+                                  Remix.arrow_right_s_line,
+                                  color: Colors.white,
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        children: [
+                          Row(children: [
+                            ClipRRect(
+                              borderRadius:
+                                  BorderRadius.circular(100), // Image border
+                              child: SizedBox.fromSize(
+                                size: const Size.fromRadius(15), // Image radius
+                                child: Image.network(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 12),
+                            const Text(
+                              'Mighty Dragons',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                  color: Colors.black),
+                              textAlign: TextAlign.left,
+                            )
+                          ]),
+                          const SizedBox(height: 15),
+                          Row(children: [
+                            ClipRRect(
+                              borderRadius:
+                                  BorderRadius.circular(100), // Image border
+                              child: SizedBox.fromSize(
+                                size: const Size.fromRadius(15), // Image radius
+                                child: Image.network(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 12),
+                            const Text(
+                              'Mighty Dragons',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                  color: Colors.black),
+                              textAlign: TextAlign.left,
+                            )
+                          ])
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                  ]),
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            const Text(
               "History",
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 19),
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              // height: 100,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(8)),
+              child: Material(
+                type: MaterialType.transparency,
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.black26,
+                  child: Column(mainAxisSize: MainAxisSize.min, children: [
+                    Container(
+                      width: double.infinity,
+                      height: 40,
+                      decoration: const BoxDecoration(
+                        color: secondaryTextColor,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(8.0),
+                          topLeft: Radius.circular(8.0),
+                        ),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Remix.basketball_fill,
+                                      color: Colors.white,
+                                      size: 18,
+                                    ),
+                                    SizedBox(width: 8),
+                                    Text(
+                                      "10/12/2023",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ],
+                                ),
+                                Spacer(),
+                                Icon(
+                                  Remix.arrow_right_s_line,
+                                  color: Colors.white,
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        children: [
+                          Row(children: [
+                            ClipRRect(
+                              borderRadius:
+                                  BorderRadius.circular(100), // Image border
+                              child: SizedBox.fromSize(
+                                size: const Size.fromRadius(15), // Image radius
+                                child: Image.network(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 12),
+                            const Text(
+                              'Mighty Dragons',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                  color: Colors.black),
+                              textAlign: TextAlign.left,
+                            )
+                          ]),
+                          const SizedBox(height: 15),
+                          Row(children: [
+                            ClipRRect(
+                              borderRadius:
+                                  BorderRadius.circular(100), // Image border
+                              child: SizedBox.fromSize(
+                                size: const Size.fromRadius(15), // Image radius
+                                child: Image.network(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 12),
+                            const Text(
+                              'Mighty Dragons',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                  color: Colors.black),
+                              textAlign: TextAlign.left,
+                            )
+                          ])
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                  ]),
+                ),
+              ),
             ),
           ],
         ),
